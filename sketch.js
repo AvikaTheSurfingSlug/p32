909192bc0fbcf445f06c22b4afd22bc2871e8607
AvikaTheSurfingSlug <77532027+AvikaTheSurfingSlug@users.noreply.github.com>
2021-04-10 14:56:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundIMG();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response = await fetch("http://worldtimeapi.org/api/timezone/asia/kolkata");
+    var responseJson = await response.json();
+    var dateTime = responseJson.datetime;
+    var HOUR = dateTime.slice(11,13)
+    console.log (HOUR);
+    
+    //change the data in JSON format
+
+    // write code slice the datetime
+
+
+    // add conditions to change the background images from sunrise to sunset
+    if(hour>=04 && hour<=06 ){
+        bg = "sunrise1.png"
+    }else if (hour>=06 && hour<=08){
+        bg = "sunrise2.png"
+    }else if (hour>=08 && hour<10){
+        bg = "sunrise3.png"
+    }else if (hour>=10 && hour<14){
+        bg = "sunrise4.png"
+    }else if (hour>=14 && hour<18){
+        bg = "sunrise5.png"
+    }else if (hour>=18 && hour<19){
+        bg = "sunset7.png"
+    }else if (hour>=19 && hour<20){
+        bg = "sunset8.png"
+    }else if (hour>=20&& hour<21){
+        bg = "sunset9.png"
+    }else if (hour>=21 && hour<22){
+        bg = "sunset10.png"
+    }else if (hour>22 && hour<23){
+        bg = "sunset11.png"
+    }else if(hour>23 && hour<04)
+        bg = "sunset12.png"
+    
+    //load the image in backgroundImg variable here
+    backgroundImg = loadImage(bg)
+}
